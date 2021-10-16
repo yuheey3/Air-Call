dec5fa4a1378f5017d97353088fcbae9d6ba8f67
unknown <yuheey3@yahoo.co.jp>
2021-10-16 14:53:42 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './styles/body.css';
+import './styles/header.css';
+import Header from './components/header.js';
+import Footer from './components/footer.js';
+
+function App() {
+  return (
+    <div className='container'>
+      <Header/>
+      <div className="container-view">Some activities should be here</div>
+      <Footer/>
+    </div>
+  );
+};
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PhoneIcon from '@mui/icons-material/Phone';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import SettingsApplicationsOutlinedIcon from '@mui/icons-material/SettingsApplicationsOutlined';
+
+
+const Footer = () => {
+  return (
+    <Footer>
+     <PhoneIcon /><PersonOutlineIcon /><SettingsApplicationsOutlinedIcon /> 
+     
+      </Footer>
+  );
+};
+
+export default Footer;
