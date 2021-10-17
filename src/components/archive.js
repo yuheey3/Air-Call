32e7ae1e7a64aa1fa8852f621773684c15bec411
unknown <yuheey3@yahoo.co.jp>
2021-10-17 03:47:30 -0400

    added archived paGE

--- src/components/archive.js
+++ src/components/archive.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Divider from '@mui/material/Divider';
+import PhoneCallbackIcon from '@mui/icons-material/PhoneCallback';
+import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
+import Grid from '@mui/material/Grid';
 
-const Archive = () => {
-  return (
-    <div className="archive">This is Archive page</div>
-  );
-};
+
+class Archive extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+
+    }
+
+    componentDidMount() {
+        fetch('https://aircall-job.herokuapp.com/activities')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
+    }
+
+
+    render() {
+        //declare item
+        var { isLoaded } = this.state;
+
+        //filter archived
+        const archiveItems = this.state.items.filter(item => {
+            return (item.is_archived === true);
+        });
+
+
+        if (!isLoaded) {
+            return <div>Loading...</div>
+        }
+
+        return (
+            <div className="activity">
+                <br />
+                <div className="card" >
+                    <div className="icon">
+                        <ArchiveOutlinedIcon />
+                    </div>
+                    <p className="title">Archive all calls</p>
+                </div>
+
+
+                {archiveItems.map(item => (
+
+                    <ul key={item.id} onClick={() => { this.props.history.push(`/detail/${item.id}`) }}>
+                        <br />
+
+                        <Divider> {new Date(item.created_at).toLocaleDateString()}</Divider><br />
+                        <div className="card">
+                            <Grid>
+                                <div className="icon">
+                                    <PhoneCallbackIcon />
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} sm container>
+                                <span className="title"> {item.from} <br /></span>
+                                tried to call on {item.via}<br />
+                            </Grid>
+                            <Grid>
+                                <span className="time">
+                                    {new Date(item.created_at).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true })}
+                                </span>
+                            </Grid>
+                        </div>
+
+                    </ul>
+                ))};
+            </div>
+        )
+    }
+}
 
 export default Archive;
